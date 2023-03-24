5e0fc5be7e5165b36b190e500e771cf2c8e24b14
JBreitenbr <119942939+JBreitenbr@users.noreply.github.com>
2023-03-24 07:20:07 +0100

    Update str.py

--- str.py
+++ str.py
@@ -1,18 +1,8 @@
 import streamlit as st
-st.title("Das ist eine Textzeile")
-st.markdown("""<button>Ein Button</button>""",True)
-
-st.markdown("""## Kleiner Versuch mit Markdown und HTML <br>
-<button> Knopf zum Abschalten von Lärm </button>""", True)
-st.markdown("""### Versuch hat bisher geklappt <br>
-<form><label for='name'><input tye='text' id='name'>Name</label></form> """, True)
 
 import pandas as pd
 from sklearn import datasets
 from sklearn.ensemble import RandomForestClassifier
-
-
-# In[2]:
 
 
 st.write("""
@@ -22,9 +12,6 @@ This app predicts the **Iris flower** type!
 
 
 st.sidebar.header('User Input Parameters')
-
-
-# In[4]:
 
 
 sepal_length = st.slider('Sepal length', 4.3, 7.9, 5.4)
@@ -42,52 +29,29 @@ features = pd.DataFrame(data, index=[0])
 df = features
 
 
-# In[6]:
-
-
 st.subheader('User Input parameters')
 st.write(df)
-
-
-# In[7]:
-
 
 iris = datasets.load_iris()
 X = iris.data
 Y = iris.target
 
 
-# In[8]:
-
-
 clf = RandomForestClassifier()
 clf.fit(X, Y)
-
-
-# In[9]:
 
 
 prediction = clf.predict(df)
 prediction_proba = clf.predict_proba(df)
 
 
-# In[10]:
-
-
 st.subheader('Class labels and their corresponding index number')
 st.write(iris.target_names)
 
 
-# In[11]:
-
-
 st.subheader('Prediction')
 st.write(iris.target_names[prediction])
-#st.write(prediction)
-
-
-# In[12]:
-
+st.write(prediction)
 
 st.subheader('Prediction Probability')
 st.write(prediction_proba)
